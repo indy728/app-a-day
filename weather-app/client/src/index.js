4025acb10664f16caae8cc64a7dff3abf455f2cf
Kyle Murray <kdevlinmurray@gmail.com>
2020-07-20 19:53:52 -0700

    pushing build to git

--- weather-app/client/src/index.js
+++ weather-app/client/src/index.js
@@ -6,7 +6,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: `http://localhost:4200/graphql`
+  uri: `/graphql`
 })
 
 const app = (
---
+++ weather-app/public/precache-manifest.3aa24a1ced034601d07b742892ad32fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565e73a19ce79c73d4aebb88d01be3b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf1d767fd1a3b2833e2",
+    "url": "/static/js/2.8e538262.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8e538262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a45e0b5f6c671d32a9",
+    "url": "/static/js/main.669f04e9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
