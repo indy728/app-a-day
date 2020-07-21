9d09a6111e4ea37d2ebb8ede7de269110565e91a
Kyle Murray <kdevlinmurray@gmail.com>
2020-07-20 19:57:52 -0700

    updating static load

--- weather-app/server.js
+++ weather-app/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schema.js');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +13,12 @@ app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const PORT = process.env.PORT || 4200;
 
