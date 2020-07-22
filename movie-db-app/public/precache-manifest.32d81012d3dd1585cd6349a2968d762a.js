7b7fd89a005c0340fa9af38ff9cec6863e056282
Kyle Murray <kdevlinmurray@gmail.com>
2020-07-22 11:42:53 -0700

    second try pushing movie app

---
+++ movie-db-app/public/precache-manifest.32d81012d3dd1585cd6349a2968d762a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f720e09b3da2de47573c48af454f429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e129a5302abb8fa101d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71607ab15d03903fca46",
+    "url": "/static/js/2.b386ed0f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.b386ed0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e129a5302abb8fa101d",
+    "url": "/static/js/main.4af959f1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
